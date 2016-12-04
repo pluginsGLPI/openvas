be9b149de217091ceade4b11b1224ac5d6476eda
Walid <wnouh@teclib.com>
2016-12-04 12:48:48 +0100

    Fix headers

--- front/item.form.php
+++ front/item.form.php
@@ -1,35 +1,33 @@
 <?php
-/*
-/*
- * @version $Id$
- LICENSE
+/* @version $Id$
+--------------------------------------------------------------------------
+LICENSE
 
-  This file is part of the openvas plugin.
+ This file is part of the openvas plugin.
 
- OpenVAS plugin is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 3 of the License, or
- (at your option) any later version.
+OpenVAS plugin is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
 
- openvas plugin is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+openvas plugin is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with GLPI; along with openvas. If not, see <http://www.gnu.org/licenses/>.
- --------------------------------------------------------------------------
- @package   openvas
- @author    Teclib'
- @copyright Copyright (c) 2016 Teclib'
- @license   GPLv3
-            http://www.gnu.org/licenses/gpl.txt
- @link      https://github.com/pluginsGLPI/openvas
- @link      http://www.glpi-project.org/
- @link      http://www.teclib-edition.com/
- @since     2016
- ---------------------------------------------------------------------- */
- ---------------------------------------------------------------------- */
+You should have received a copy of the GNU General Public License
+along with GLPI; along with openvas. If not, see <http://www.gnu.org/licenses/>.
+--------------------------------------------------------------------------
+@package   openvas
+@author    Teclib'
+@copyright Copyright (c) 2016 Teclib'
+@license   GPLv3
+           http://www.gnu.org/licenses/gpl.txt
+@link      https://github.com/pluginsGLPI/openvas
+@link      http://www.glpi-project.org/
+@link      http://www.teclib-edition.com/
+@since     2016
+----------------------------------------------------------------------*/
 
 include ("../../../inc/includes.php");
 
