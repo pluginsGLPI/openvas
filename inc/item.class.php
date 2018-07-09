c560f0e11820cd52c558541c7f3c48bbee1936c6
tsmr <thetsmr@gmail.com>
2018-07-09 18:14:57 +0200

    Fix query

--- inc/item.class.php
+++ inc/item.class.php
@@ -652,7 +652,7 @@ class PluginOpenvasItem extends CommonDBChild {
       //Clean items without openvas id
       $query = "SELECT `id`
                 FROM `glpi_plugin_openvas_items`
-                WHERE `openvas_id` = '0'";
+                WHERE `openvas_id` = 'N/A'";
       foreach ($DB->request($query) as $target) {
          $tmp = ['id'               => $target['id']
                 ];
